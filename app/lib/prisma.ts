d935e48af2c00031571c9913666cd6a9a7e57663
Rani Gande <rani.24@kgpian.iitkgp.ac.in>
2026-07-27 20:47:26 +0530

    Fix Prisma generation for Vercel deployment

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,37 +1,15 @@
-import "dotenv/config";
+import { PrismaClient } from "@prisma/client";
 
-import { PrismaClient } from "@/app/generated/prisma";
-import { PrismaPg } from "@prisma/adapter-pg";
-
-
-const connectionString = process.env.DATABASE_URL;
-
-
-if (!connectionString) {
-  throw new Error("DATABASE_URL is missing");
-}
-
-
-const adapter = new PrismaPg({
-  connectionString,
-});
-
-
-const globalForPrisma = global as unknown as {
+const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
-
 
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter,
+    log: ["query"],
   });
-
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
-
-
-export default prisma;
